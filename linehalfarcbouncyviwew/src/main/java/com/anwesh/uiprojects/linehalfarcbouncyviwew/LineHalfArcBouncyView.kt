0bf9890d63c49885e473ad7ab66864b8126bf9c5
Anwesh43 <anweshthecool0@gmail.com>
2020-01-03 11:19:46 +0530

    adding LineHalfArcBouncyView class

--- linehalfarcbouncyviwew/src/main/java/com/anwesh/uiprojects/linehalfarcbouncyviwew/LineHalfArcBouncyView.kt
+++ linehalfarcbouncyviwew/src/main/java/com/anwesh/uiprojects/linehalfarcbouncyviwew/LineHalfArcBouncyView.kt
@@ -53,3 +53,21 @@ fun Canvas.drawLHANode(i : Int, scale : Float, paint : Paint) {
     drawLineHalfArcs(scale, size, h / 2, paint)
     restore()
 }
+
+class LineHalfArcBouncyView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
