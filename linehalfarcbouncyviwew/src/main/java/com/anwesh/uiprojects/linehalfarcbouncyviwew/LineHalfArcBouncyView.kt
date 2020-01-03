f338a8865d4792d6da2a3e8b34c7239cb4911bb6
Anwesh43 <anweshthecool0@gmail.com>
2020-01-03 11:11:15 +0530

    adding constants and libraries

--- linehalfarcbouncyviwew/src/main/java/com/anwesh/uiprojects/linehalfarcbouncyviwew/LineHalfArcBouncyView.kt
+++ linehalfarcbouncyviwew/src/main/java/com/anwesh/uiprojects/linehalfarcbouncyviwew/LineHalfArcBouncyView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.linehalfarcbouncyviwew
 /**
  * Created by anweshmishra on 03/01/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.02f / lines
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.CYAN
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+
