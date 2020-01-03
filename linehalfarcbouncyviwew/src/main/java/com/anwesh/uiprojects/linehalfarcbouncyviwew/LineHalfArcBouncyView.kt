b9ab0aacc100963728c16e6b212331154bdaed22
Anwesh43 <anweshthecool0@gmail.com>
2020-01-03 11:18:21 +0530

    adding Canvas extension methods

--- linehalfarcbouncyviwew/src/main/java/com/anwesh/uiprojects/linehalfarcbouncyviwew/LineHalfArcBouncyView.kt
+++ linehalfarcbouncyviwew/src/main/java/com/anwesh/uiprojects/linehalfarcbouncyviwew/LineHalfArcBouncyView.kt
@@ -25,3 +25,31 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse()).toFloat()
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawLineHalfArc(i : Int, scale : Float, size : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify().divideScale(i, lines)
+    save()
+    scale(1f, 1f - 2 * i)
+    drawLine(0f, h , 0f, h + (size - h) * sf, paint)
+    restore()
+}
+
+fun Canvas.drawLineHalfArcs(scale : Float, size : Float, h : Float, paint : Paint) {
+    for (j in 0..(lines - 1)) {
+        drawLineHalfArc(j, scale, size, h, paint)
+    }
+}
+
+fun Canvas.drawLHANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(gap * (i + 1), h / 2)
+    drawLineHalfArcs(scale, size, h / 2, paint)
+    restore()
+}
